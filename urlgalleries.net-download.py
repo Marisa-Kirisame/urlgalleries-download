1eee9d36a556dcf76af96419f8416e8e505b8af5
Marisa-Kirisame <Marisa-Kirisame@protonmail.ch>
2018-04-13 08:03:46 -0500

    Working

--- urlgalleries.net-download.py
+++ urlgalleries.net-download.py
@@ -11,9 +11,7 @@ from bs4 import BeautifulSoup
 from parse import compile
 
 force = False
-
-url = 'http://moggy.urlgalleries.net/blog_gallery.php?id=4873870&g=Isabella_C_in_Diavgia'
-dir = 'Isabella C in Diavgia'
+file_number = None
 
 def parse_baseurl_url_query(url_dirty):
     """Return the baseurl, url, and query."""
@@ -30,7 +28,7 @@ def parse_baseurl(url):
 
 p = compile("linkDestUrl = '{}';\n")
 
-def get_img_src(url):
+def get_img_src1(url):
     _, url_clean, query = parse_baseurl_url_query(url)
     r = requests.get(url_clean, params=query)
     r.raise_for_status()
@@ -55,12 +53,58 @@ def get_img_src(url):
 
     return urljoin(baseurl, img_src)
 
+p2 = compile("location.href='{}';")
+
+def get_img_src2(url):
+    _, url_clean, query = parse_baseurl_url_query(url)
+    r = requests.get(url_clean, params=query)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    input_link = soup.find('input', id='btnContinue')['onclick']
+    img_url = p2.search(input_link)
+    if img_url is None:
+        return None
+    else:
+        img_url = img_url[0]
+
+    baseurl, img_url, query = parse_baseurl_url_query(img_url)
+    r = requests.get(img_url, params=query)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    img = soup.find('img', id='thepic')
+    if img is None:
+        return None
+    img_src = img['src']
+
+    return urljoin(baseurl, img_src)
+
 def save_file(url, file_name):
     r = requests.get(url, allow_redirects=True)
     r.raise_for_status()
 
     with open(file_name, 'wb') as file_:
         file_.write(r.content)
+
+def save_from_img_link(baseurl, img_link, file_name_base):
+    url = urljoin(baseurl, img_link)
+    try:
+        img_src = get_img_src1(url)
+        if img_src is None:
+            img_src = get_img_src2(url)
+    except KeyboardInterrupt:
+        raise
+    except:
+        print('Error: ' + url)
+        return
+    if img_src is not None:
+        ext = img_src.rsplit('.', 1)[-1]
+        file_name = file_name_base + '.' + ext
+        save_file(img_src, file_name)
+        print(file_name)
+    else:
+        print('Error: ' + url)
 
 def get_images(url, dir):
     if not os.path.exists(dir):
@@ -77,30 +121,22 @@ def get_images(url, dir):
 
     soup = BeautifulSoup(r.text, 'lxml')
     img_links = soup.find('td', attrs={'class': 'gallerybody'}).table.find_all('a')
-    for x, img_link in enumerate(img_links, 1):
-        if not force and glob.glob(os.path.join(dir, '{}.*'.format(x))):
-            continue
-
-        url = urljoin(baseurl, img_link['href'])
+    if file_number is not None:
         try:
-            img_src = get_img_src(url)
-        except KeyboardInterrupt:
-            raise
-        except:
-            print('Error: ' + img_link['href'])
-        if img_src is not None:
-            ext = img_src.rsplit('.', 1)[-1]
-            file_name = os.path.join(dir, str(x) + '.' + ext)
-            save_file(img_src, file_name)
-            print(file_name)
-        else:
-            print('Error: ' + img_link['href'])
+            img_link = img_links[file_number-1]
+        except IndexError:
+            print('Invalid file number {} -- the number of files is {}'
+                  .format(file_number, len(img_links)), file=sys.stderr)
 
-def test():
-    try:
-        get_images(url, dir)
-    except FileExistsError:
-        sys.exit(1)
+        file_name_base = os.path.join(dir, str(file_number))
+        save_from_img_link(baseurl, img_link['href'], file_name_base)
+    else:
+        for x, img_link in enumerate(img_links, 1):
+            if not force and glob.glob(os.path.join(dir, '{}.*'.format(x))):
+                continue
+
+            file_name_base = os.path.join(dir, str(x))
+            save_from_img_link(baseurl, img_link['href'], file_name_base)
 
 def main():
     parser = argparse.ArgumentParser(description='''Download images from
@@ -111,11 +147,16 @@ def main():
                         help='The output directory. Default is the current directory')
     parser.add_argument('-f', '--force', dest='force', action='store_true',
                         help='Force overwriting image files')
+    parser.add_argument('-n', '--number', dest='file_number', type=int,
+                        help='Only download <number>.jpg')
     args = parser.parse_args()
 
     if args.force:
         global force
         force = True
+    if args.file_number:
+        global file_number
+        file_number = args.file_number
 
     try:
         get_images(args.url, args.output_folder)
@@ -123,5 +164,4 @@ def main():
         sys.exit(1)
 
 if __name__ == "__main__":
-    #test()
     main()
